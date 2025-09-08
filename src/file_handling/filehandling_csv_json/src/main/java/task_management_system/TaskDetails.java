6653b195dbe0c003f1475120c47c207634b72926
Prasanna <prasannahanchinamani.15@gmail.com>
2025-09-08 12:59:01 +0530

    Delete src/file_handling directory

--- src/file_handling/filehandling_csv_json/src/main/java/task_management_system/TaskDetails.java
+++
@@ -1,8 +0,0 @@
-package file_handling.filehandling_csv_json.src.main.java.task_management_system;
-
-public class TaskDetails {
-    private int TaskID;
-    private String Description;
-    private String DueDate;
-    private boolean Completed;
-}
